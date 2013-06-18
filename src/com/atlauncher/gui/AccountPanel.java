2d8788f51a269511089f77e90f88394eeb838b43
RyanTheAllmighty <rldowling03@gmail.com>
2013-06-19 01:05:53 +0930

    Start of layout for AccountsPanel

--- src/com/atlauncher/gui/AccountPanel.java
+++ src/com/atlauncher/gui/AccountPanel.java
@@ -10,16 +10,109 @@
  */
 package com.atlauncher.gui;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import com.atlauncher.data.Account;
 
 public class AccountPanel extends JPanel {
 
+    private JLabel userSkin;
+    private JPanel rightPanel;
+    private JPanel topPanel;
+    private JComboBox<Account> accountsComboBox;
+    private JPanel bottomPanel;
+
+    private final Insets TOP_INSETS = new Insets(0, 0, 20, 0);
+    private final Insets BOTTOM_INSETS = new Insets(20, 0, 0, 0);
+    private final Insets LABEL_INSETS = new Insets(3, 0, 3, 10);
+    private final Insets FIELD_INSETS = new Insets(3, 0, 3, 0);
+
     public AccountPanel() {
-        setLayout(new FlowLayout());
-        add(new JLabel(LauncherFrame.settings.getAccounts().get(0).getMinecraftSkin()));
+        setLayout(new BorderLayout());
+
+        rightPanel = new JPanel();
+        rightPanel.setLayout(new BorderLayout());
+
+        topPanel = new JPanel();
+
+        bottomPanel = new JPanel();
+        bottomPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.insets = TOP_INSETS;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        accountsComboBox = new JComboBox<Account>();
+        accountsComboBox.addItem(new Account("", "", "Select A Username", false));
+        for (Account account : LauncherFrame.settings.getAccounts()) {
+            accountsComboBox.addItem(account);
+        }
+        accountsComboBox.setSelectedIndex(0);
+        accountsComboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(accountsComboBox.getSelectedIndex()==0){
+                    userSkin.setIcon(null);
+                }else{
+                    Account account = (Account) accountsComboBox.getSelectedItem();
+                    userSkin.setIcon(account.getMinecraftSkin());
+                }
+            }
+        });
+        bottomPanel.add(accountsComboBox, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 1;
+        gbc.insets = LABEL_INSETS;
+        gbc.anchor = GridBagConstraints.BASELINE_TRAILING;
+        bottomPanel.add(new JLabel("Username:"), gbc);
+
+        gbc.gridx++;
+        gbc.insets = FIELD_INSETS;
+        gbc.anchor = GridBagConstraints.BASELINE_LEADING;
+        bottomPanel.add(new JTextField(16), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.insets = LABEL_INSETS;
+        gbc.anchor = GridBagConstraints.BASELINE_TRAILING;
+        bottomPanel.add(new JLabel("Password:"), gbc);
+
+        gbc.gridx++;
+        gbc.insets = FIELD_INSETS;
+        gbc.anchor = GridBagConstraints.CENTER;
+        bottomPanel.add(new JPasswordField(16), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 2;
+        gbc.insets = BOTTOM_INSETS;
+        gbc.anchor = GridBagConstraints.CENTER;
+        bottomPanel.add(new JButton("Save"), gbc);
+
+        rightPanel.add(topPanel, BorderLayout.NORTH);
+        rightPanel.add(bottomPanel, BorderLayout.CENTER);
+
+        userSkin = new JLabel(LauncherFrame.settings.getAccounts().get(0).getMinecraftSkin());
+        userSkin.setBorder(BorderFactory.createEmptyBorder(0, 60, 0, 0));
+        add(userSkin, BorderLayout.WEST);
+        add(rightPanel, BorderLayout.CENTER);
     }
 
 }
