e7e5c9cd35c4f1ca9fcd34a6e093724851dced61
RyanTheAllmighty <rldowling03@gmail.com>
2013-07-21 01:11:11 +0930

    3.0.2 - Fixed German display (Thanks to Togetherfreaks3 for the translation)

--- src/com/atlauncher/gui/LauncherFrame.java
+++ src/com/atlauncher/gui/LauncherFrame.java
@@ -49,7 +49,11 @@ public class LauncherFrame extends JFrame {
         App.settings.getConsole().log("Made By Bob*");
         App.settings.getConsole().log("*(Not Actually)");
         App.settings.setParentFrame(this);
-        setSize(new Dimension(800, 500));
+        if (App.settings.getLanguage().getName().equalsIgnoreCase("German")) {
+            setSize(new Dimension(875, 500));
+        } else {
+            setSize(new Dimension(800, 500));
+        }
         setTitle("ATLauncher %VERSION%");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
