71be8f0f46f8012081d48f3c24966468dfd587f1
RyanTheAllmighty <rldowling03@gmail.com>
2013-11-04 17:11:41 +1100

    Download forge versions list

--- src/com/atlauncher/data/Settings.java
+++ src/com/atlauncher/data/Settings.java
@@ -358,6 +358,13 @@ public class Settings {
             while (!executor.isTerminated()) {
             }
         }
+        File localForgeList = new File(getConfigsDir(), "forgeList.json");
+        Downloadable remoteForgeList = new Downloadable(
+                "http://files.minecraftforge.net/minecraftforge/json", localForgeList, "-", null,
+                false);
+        if (remoteForgeList.getFilesize() != localForgeList.length()) {
+            remoteForgeList.download(false);
+        }
     }
 
     /**
@@ -373,6 +380,13 @@ public class Settings {
             if (download.needToDownload()) {
                 return true; // 1 file needs to be updated so there is updated files
             }
+        }
+        File localForgeList = new File(getConfigsDir(), "forgeList.json");
+        Downloadable remoteForgeList = new Downloadable(
+                "http://files.minecraftforge.net/minecraftforge/json", localForgeList, "-", null,
+                false);
+        if (remoteForgeList.getFilesize() != localForgeList.length()) {
+            return true;
         }
         return false; // No updates
     }
