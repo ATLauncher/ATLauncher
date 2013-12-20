39d1977cfb6d88a1b99867c0b2cbd49cfce7045f
RyanTheAllmighty <rldowling03@gmail.com>
2013-12-21 10:52:40 +1100

    *cough*

--- src/com/atlauncher/data/Instance.java
+++ src/com/atlauncher/data/Instance.java
@@ -14,7 +14,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Locale;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
--- src/com/atlauncher/data/Language.java
+++ src/com/atlauncher/data/Language.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Locale;
 import java.util.Properties;
 
 import com.atlauncher.App;
