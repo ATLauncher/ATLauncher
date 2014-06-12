b1d51bab5aef477c50c6250d218b6b27b9dd13d5
Asyncronous <asyncronous16@gmail.com>
2014-06-12 18:10:16 -0400

    Rewrote NewsTab.java

--- src/main/java/com/atlauncher/gui/tabs/NewsTab.java
+++ src/main/java/com/atlauncher/gui/tabs/NewsTab.java
@@ -8,7 +8,6 @@ package com.atlauncher.gui.tabs;
 
 import java.awt.BorderLayout;
 import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -31,105 +30,78 @@ import com.atlauncher.utils.Utils;
  * This class extends {@link JPanel} and provides a Panel for displaying the latest news.
  */
 public class NewsTab extends JPanel {
-
     /**
      * Auto generated serial.
      */
     private static final long serialVersionUID = 4616284541226058793L;
 
+    private final HTMLEditorKit NEWS_KIT = new HTMLEditorKit(){
+        {
+            this.setStyleSheet(Utils.createStyleSheet("news"));
+        }
+    };
+
+    private final ContextMenu NEWS_MENU = new ContextMenu();
+
     /**
      * {@link JEditorPane} which contains all the news for this panel.
      */
-    private JEditorPane newsArea;
+    private final JEditorPane NEWS_PANE = new JEditorPane("text/html", ""){
+        {
+            this.setEditable(false);
+            this.setEditorKit(NEWS_KIT);
+            this.addHyperlinkListener(new HyperlinkListener() {
+                @Override
+                public void hyperlinkUpdate(HyperlinkEvent e) {
+                    if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED){
+                        Utils.openBrowser(e.getURL());
+                    }
+                }
+            });
+            this.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    if(NEWS_PANE.getSelectedText() != null){
+                        if(e.getButton() == MouseEvent.BUTTON3){
+                            NEWS_MENU.show(NEWS_PANE, e.getX(), e.getY());
+                        }
+                    }
+                }
+            });
+        }
+    };
 
-    /**
-     * The context menu when selected text is right clicked.
-     */
-    private JPopupMenu contextMenu;
-
-    /**
-     * The copy button used in the context menu.
-     */
-    private JMenuItem copy;
 
     /**
      * Instantiates a new instance of this class which sets the layout and loads the content.
      */
     public NewsTab() {
-        setLayout(new BorderLayout());
-        loadContent();
-    }
-
-    /**
-     * Loads the news into the content of this panel.
-     */
-    private void loadContent() {
-        newsArea = new JEditorPane("text/html", null);
-        newsArea.setEditable(false);
-        newsArea.setSelectionColor(App.THEME.getSelectionColor());
-        setupContextMenu(); // Setup the right click context menu
-
-        HTMLEditorKit kit = new HTMLEditorKit();
-        kit.setStyleSheet(Utils.createStyleSheet("news")); // Get the news StyleSheet
-        newsArea.setEditorKit(kit);
-
-        newsArea.addHyperlinkListener(new HyperlinkListener() {
-            public void hyperlinkUpdate(HyperlinkEvent e) {
-                if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-                    Utils.openBrowser(e.getURL());
-                }
-            }
-        });
-
-        add(new JScrollPane(newsArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), BorderLayout.CENTER);
-
-        newsArea.setText(App.settings.getNewsHTML()); // Load the HTML for the news
-        newsArea.setCaretPosition(0); // Reset position back to the top
-    }
-
-    /**
-     * Sets up the right click Context Menu.
-     */
-    private void setupContextMenu() {
-        contextMenu = new JPopupMenu();
-
-        copy = new JMenuItem("Copy");
-        copy.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                StringSelection text = new StringSelection(newsArea.getSelectedText());
-                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                clipboard.setContents(text, null);
-            }
-        });
-        contextMenu.add(copy);
-
-        newsArea.addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                if (newsArea.getSelectedText() != null) {
-                    if (e.getButton() == MouseEvent.BUTTON3) {
-                        contextMenu.show(newsArea, e.getX(), e.getY());
-                    }
-                }
-            }
-        });
-    }
-
-    /**
-     * Sets up the language and correct localization for the users selected langauge.
-     */
-    public void setupLanguage() {
-        copy.setText(App.settings.getLocalizedString("common.copy"));
+        super(new BorderLayout());
+        this.add(new JScrollPane(this.NEWS_PANE, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), BorderLayout.CENTER);
+        this.reload();
     }
 
     /**
      * Reloads the panel with updated news.
      */
     public void reload() {
-        removeAll();
-        loadContent();
-        validate();
-        repaint();
+        this.NEWS_PANE.setText("");
+        this.NEWS_PANE.setText(App.settings.getNewsHTML());
+        this.NEWS_PANE.setCaretPosition(0);
     }
 
+    private final class ContextMenu extends JPopupMenu {
+        private final JMenuItem COPY_ITEM = new JMenuItem("Copy");
+
+        public ContextMenu(){
+            super();
+            this.COPY_ITEM.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    StringSelection text = new StringSelection(NEWS_PANE.getSelectedText());
+                    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(text, null);
+                }
+            });
+        }
+    }
 }
