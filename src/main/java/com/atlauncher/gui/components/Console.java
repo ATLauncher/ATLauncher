4e95b96cf234a912025178ecca4780391e12e285
RyanTheAllmighty <rldowling03@gmail.com>
2014-06-16 21:17:25 +1000

    Make console scroll to bottom when text is added

--- src/main/java/com/atlauncher/gui/components/Console.java
+++ src/main/java/com/atlauncher/gui/components/Console.java
@@ -1,26 +1,35 @@
 package com.atlauncher.gui.components;
 
-import javax.swing.*;
-import javax.swing.text.*;
-import java.awt.*;
+import java.awt.Color;
 
-public final class Console extends JTextPane{
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+
+public final class Console extends JTextPane {
+
+    /**
+     * Auto generate serial.
+     */
+    private static final long serialVersionUID = 5325985090210097809L;
     private final SimpleAttributeSet attrs = new SimpleAttributeSet();
 
-    public Console(){
+    public Console() {
         this.setEditable(false);
     }
 
-    public Console setColor(Color c){
+    public Console setColor(Color c) {
         StyleConstants.setForeground(this.attrs, c);
         return this;
     }
 
-    public void write(String str){
-        try{
+    public void write(String str) {
+        try {
             this.getDocument().insertString(this.getDocument().getLength(), str, this.attrs);
-        } catch(Exception ex){
+            this.setCaretPosition(this.getDocument().getLength());
+        } catch (Exception ex) {
             ex.printStackTrace(System.err);
         }
     }
+
 }
