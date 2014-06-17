7d8172c361491f2603465b04655cb00915dbea80
RyanTheAllmighty <rldowling03@gmail.com>
2014-06-17 22:01:33 +1000

    Add Launcher Version

---
+++ src/main/java/com/atlauncher/data/LauncherVersion.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2013-2014 by ATLauncher and Contributors
+ *
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/.
+ */
+package com.atlauncher.data;
+
+public class LauncherVersion {
+    private int reserved;
+    private int major;
+    private int minor;
+    private int revision;
+
+    public LauncherVersion(int reserved, int major, int minor, int revision) {
+        this.reserved = reserved;
+        this.major = major;
+        this.minor = minor;
+        this.revision = revision;
+    }
+
+    public int getReserved() {
+        return this.reserved;
+    }
+
+    public int getMajor() {
+        return this.major;
+    }
+
+    public int getMinor() {
+        return this.minor;
+    }
+
+    public int getRevision() {
+        return this.revision;
+    }
+
+    public boolean needsUpdate(LauncherVersion toThis) {
+        if (this.reserved > toThis.getReserved()) {
+            return false;
+        } else if (this.reserved < toThis.getReserved()) {
+            return true;
+        } else {
+            if (this.major > toThis.getMajor()) {
+                return false;
+            } else if (this.major < toThis.getMajor()) {
+                return true;
+            } else {
+                if (this.minor > toThis.getMinor()) {
+                    return false;
+                } else if (this.minor < toThis.getMinor()) {
+                    return true;
+                } else {
+                    if (this.revision > toThis.getRevision()) {
+                        return false;
+                    } else if (this.revision < toThis.getRevision()) {
+                        return true;
+                    } else {
+                        return false; // Same version so doesn't need to update
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d.%d.%d.%d", this.reserved, this.major, this.minor, this.revision);
+    }
+}
---
+++ src/test/java/TestLauncherVersion.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2013-2014 by ATLauncher and Contributors
+ * 
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. To
+ * view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/.
+ */
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.atlauncher.data.Constants;
+import com.atlauncher.data.LauncherVersion;
+
+public class TestLauncherVersion {
+
+    @Test
+    public void test() {
+        // Test same version - no update
+        assertFalse(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved(), Constants.VERSION.getMajor(), Constants.VERSION.getMinor(),
+                Constants.VERSION.getRevision())));
+
+        // Test older Reserved - launcher had a big update
+        assertTrue(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved() + 1, Constants.VERSION.getMajor(), Constants.VERSION.getMinor(),
+                Constants.VERSION.getRevision())));
+
+        // Test older Major - launcher had major update
+        assertTrue(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved(), Constants.VERSION.getMajor() + 1, Constants.VERSION.getMinor(),
+                Constants.VERSION.getRevision())));
+
+        // Test older Minor - launcher had minor update
+        assertTrue(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved(), Constants.VERSION.getMajor(), Constants.VERSION.getMinor() + 1,
+                Constants.VERSION.getRevision())));
+
+        // Test older Revision - launcher had a bug fix
+        assertTrue(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved(), Constants.VERSION.getMajor(), Constants.VERSION.getMinor(),
+                Constants.VERSION.getRevision() + 1)));
+
+        // Test newer Reserved - launcher dev version
+        assertFalse(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved() - 1, Constants.VERSION.getMajor(), Constants.VERSION.getMinor(),
+                Constants.VERSION.getRevision())));
+
+        // Test newer Major - launcher dev version
+        assertFalse(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved(), Constants.VERSION.getMajor() - 1, Constants.VERSION.getMinor(),
+                Constants.VERSION.getRevision())));
+
+        // Test newer Minor - launcher dev version
+        assertFalse(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved(), Constants.VERSION.getMajor(), Constants.VERSION.getMinor() - 1,
+                Constants.VERSION.getRevision())));
+
+        // Test newer Revision - launcher dev version
+        assertFalse(Constants.VERSION.needsUpdate(new LauncherVersion(Constants.VERSION
+                .getReserved(), Constants.VERSION.getMajor(), Constants.VERSION.getMinor(),
+                Constants.VERSION.getRevision() - 1)));
+    }
+
+}
