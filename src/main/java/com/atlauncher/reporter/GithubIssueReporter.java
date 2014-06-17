3f14ed8fa939075c31c503f8912f409d6b911914
RyanTheAllmighty <rldowling03@gmail.com>
2014-06-18 03:11:52 +1000

    Only report to github if logging is enabled

--- src/main/java/com/atlauncher/reporter/GithubIssueReporter.java
+++ src/main/java/com/atlauncher/reporter/GithubIssueReporter.java
@@ -9,18 +9,21 @@ import com.atlauncher.thread.PasteUpload;
 import com.atlauncher.utils.Utils;
 
 public final class GithubIssueReporter {
-    private GithubIssueReporter() {}
+    private GithubIssueReporter() {
+    }
 
     public static void submit(String title, String body) throws Exception {
-        body = body + "\n\n" + times('-', 50) + "\n" + "Here is my log: "
-                + App.TASKPOOL.submit(new PasteUpload()).get();
-        Map<String, Object> request = new HashMap<String, Object>();
-        request.put("issue", new GithubIssue(title, body));
+        if (App.settings != null && App.settings.enableLogs()) {
+            body = body + "\n\n" + times('-', 50) + "\n" + "Here is my log: "
+                    + App.TASKPOOL.submit(new PasteUpload()).get();
+            Map<String, Object> request = new HashMap<String, Object>();
+            request.put("issue", new GithubIssue(title, body));
 
-        try {
-            Utils.sendAPICall("githubissue/", request);
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                Utils.sendAPICall("githubissue/", request);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
