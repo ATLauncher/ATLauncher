2b92c428fbd46d8db9875227c8242b4ebc867d2e
RyanTheAllmighty <rldowling03@gmail.com>
2014-06-18 04:40:40 +1000

    Fix APIResponse implementation to use the correct return format from API

--- src/main/java/com/atlauncher/data/APIResponse.java
+++ src/main/java/com/atlauncher/data/APIResponse.java
@@ -6,31 +6,82 @@
  */
 package com.atlauncher.data;
 
-import com.atlauncher.App;
-
+/**
+ * Response to an API call made to ATLauncher servers. This contains information including if there
+ * was an error, the response code of the request, the error message (if any) and the data received
+ * (if any) from the API.
+ */
 public class APIResponse {
+    /**
+     * If this request is an error or not.
+     */
     private boolean error;
+
+    /**
+     * The response code returned.
+     * 
+     * @see <a
+     *      href="http://wiki.atlauncher.com/api:response_code">http://wiki.atlauncher.com/api:response_code</a>
+     */
     private int code;
+
+    /**
+     * The error message sent back by the API. Returns null if there was no error.
+     */
     private String message;
 
+    /**
+     * The data sent back by the API (if applicable). Can be of various types and may not be
+     * specified at all.
+     */
+    private Object data;
+
+    /**
+     * Checks if this response was an error or not.
+     * 
+     * @return if there was an error or not
+     */
     public boolean wasError() {
         return this.error;
     }
 
+    /**
+     * Gets the response code for this API response.
+     * 
+     * @return the response code
+     * @see <a
+     *      href="http://wiki.atlauncher.com/api:response_code">http://wiki.atlauncher.com/api:response_code</a>
+     */
     public int getCode() {
         return this.code;
     }
 
+    /**
+     * Gets the error message returned by the API.
+     * 
+     * @return the error message received from the API if there was an error, or null if there was
+     *         no error
+     */
     public String getMessage() {
         return this.message;
     }
 
-    public int getMessageAsInt() {
-        try {
-            return Integer.parseInt(this.message);
-        } catch (NumberFormatException e) {
-            App.settings.logStackTrace(e);
-            return 0;
-        }
+    /**
+     * Gets the data returned by the API.
+     * 
+     * @return the data returned from the API. Please note that this may not be set or be null.
+     */
+    public Object getData() {
+        return this.data;
+    }
+
+    /**
+     * Gets the data returned by the API as an Integer. If the data is not able to be parsed as an
+     * Integer, -1 is returned.
+     * 
+     * @return the Integer of the message/response or -1 if it couldn't be converted
+     */
+    public int getDataAsInt() {
+        return (Integer) this.data;
     }
 }
