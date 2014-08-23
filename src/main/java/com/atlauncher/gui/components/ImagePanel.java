abd54c20738f56092a8f1b4e93947cb98c36b95a
RyanTheAllmighty <rldowling03@gmail.com>
2014-08-23 15:23:40 +1000

    Fix stretched images on the ImagePanel

--- src/main/java/com/atlauncher/gui/components/ImagePanel.java
+++ src/main/java/com/atlauncher/gui/components/ImagePanel.java
@@ -1,23 +1,22 @@
 package com.atlauncher.gui.components;
 
+import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import javax.swing.JPanel;
 
-public final class ImagePanel
-extends JPanel{
+public final class ImagePanel extends JPanel {
     private final Image image;
 
-    public ImagePanel(Image image){
+    public ImagePanel(Image image) {
         this.image = image;
         this.setPreferredSize(new Dimension(image.getWidth(null), image.getHeight(null)));
     }
 
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(this.image, 0, 0, this.getWidth(), this.getHeight(), null);
+        g2.drawImage(this.image, 0, (this.getHeight() - 150) / 2, 300, 150, null);
     }
 }
