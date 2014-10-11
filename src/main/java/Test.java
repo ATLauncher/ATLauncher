bd98a0182be57bbb1842860eb61c01d05acdb95b
Jamie (Lexteam) <lexwaresupport@outlook.com>
2014-10-11 22:32:20 +0100

    Removed old Test class.

--- src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-public final class Test {
-    public static void main(String... args) throws Exception {
-        // Removed due to port binding issues causing non loading clients
-        // Registry registry = RMIRegistry.local();
-        // RMIInvokerProxy relauncher = RMIRegistry.lookup(registry, "atl-relauncher");
-        // relauncher.invoke();
-    }
-}
