4736e446afcb9700437ec53239e1f09daac3f5e0
RyanTheAllmighty <rldowling03@gmail.com>
2014-10-18 18:37:08 +1100

    Add in update and server install logging seperately to pack installs and also don't send off play time requests if it's 0 since the server rejects it anyway

--- src/main/java/com/atlauncher/data/Pack.java
+++ src/main/java/com/atlauncher/data/Pack.java
@@ -911,4 +911,32 @@ public class Pack {
         }
         return "Install Not Added!";
     }
+
+    public String addServerInstall(String version) {
+        Map<String, Object> request = new HashMap<String, Object>();
+
+        request.put("username", App.settings.getAccount().getMinecraftUsername());
+        request.put("version", version);
+
+        try {
+            return Utils.sendAPICall("pack/" + getSafeName() + "/serverinstalled/", request);
+        } catch (IOException e) {
+            App.settings.logStackTrace(e);
+        }
+        return "Install Not Added!";
+    }
+
+    public String addUpdate(String version) {
+        Map<String, Object> request = new HashMap<String, Object>();
+
+        request.put("username", App.settings.getAccount().getMinecraftUsername());
+        request.put("version", version);
+
+        try {
+            return Utils.sendAPICall("pack/" + getSafeName() + "/updated/", request);
+        } catch (IOException e) {
+            App.settings.logStackTrace(e);
+        }
+        return "Install Not Added!";
+    }
 }
