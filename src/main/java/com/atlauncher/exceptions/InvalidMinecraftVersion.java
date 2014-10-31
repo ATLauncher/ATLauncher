4867bb4b2cfac7edabd7c72f7dac6522e0eb3c59
RyanTheAllmighty <rldowling03@gmail.com>
2014-11-01 01:11:22 +1100

    Update license info in header files. Thankyou IntelliJ for making that dead simple

--- src/main/java/com/atlauncher/exceptions/InvalidMinecraftVersion.java
+++ src/main/java/com/atlauncher/exceptions/InvalidMinecraftVersion.java
@@ -1,8 +1,19 @@
-/**
- * Copyright 2013 and onwards by ATLauncher and Contributors
+/*
+ * ATLauncher - https://github.com/ATLauncher/ATLauncher
+ * Copyright (C) 2013 ATLauncher
  *
- * This work is licensed under the GNU General Public License v3.0.
- * Link to license: http://www.gnu.org/licenses/gpl-3.0.txt.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.atlauncher.exceptions;
 
--- src/main/java/com/atlauncher/gui/card/InstanceCard.java
+++ src/main/java/com/atlauncher/gui/card/InstanceCard.java
@@ -1,9 +1,21 @@
-/**
- * Copyright 2013 and onwards by ATLauncher and Contributors
+/*
+ * ATLauncher - https://github.com/ATLauncher/ATLauncher
+ * Copyright (C) 2013 ATLauncher
  *
- * This work is licensed under the GNU General Public License v3.0.
- * Link to license: http://www.gnu.org/licenses/gpl-3.0.txt.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.atlauncher.gui.card;
 
 import com.atlauncher.App;
@@ -172,7 +184,7 @@ public class InstanceCard extends CollapsiblePanel implements RelocalizationList
                     String[] options = {Language.INSTANCE.localize("common.yes"), Language.INSTANCE.localize("common"
                             + ".no"), Language.INSTANCE.localize("instance.dontremindmeagain")};
                     int ret = JOptionPane.showOptionDialog(App.settings.getParent(),
-                            "<html><p align=\"center\">" + Language.INSTANCE.localizeWithReplace("instance" +
+                            "<html><p align=\"center\">" + Language.INSTANCE.localizeWithReplace("instance" + "" +
                                     ".updatenow", "<br/><br/>") + "</p></html>",
                             Language.INSTANCE.localize("instance" + "" +
                                     ".updateavailable"), JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null,
