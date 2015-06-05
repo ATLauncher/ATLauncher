368808d824fae7a692c0d797bc29173e529d3ff5
RyanTheAllmighty <ryantheallmighty@atlauncher.com>
2015-06-05 23:45:26 +1000

    Fix the parsing of some command line arguments

--- src/main/java/com/atlauncher/App.java
+++ src/main/java/com/atlauncher/App.java
@@ -17,60 +17,23 @@
  */
 package com.atlauncher;
 
-import com.atlauncher.data.Constants;
-import com.atlauncher.data.Instance;
 import com.atlauncher.data.OldSettings;
-import com.atlauncher.data.Pack;
 import com.atlauncher.evnt.EventModule;
 import com.atlauncher.gui.LauncherConsole;
 import com.atlauncher.gui.LauncherFrame;
-import com.atlauncher.gui.SplashScreen;
 import com.atlauncher.gui.TrayMenu;
-import com.atlauncher.gui.dialogs.SetupDialog;
 import com.atlauncher.gui.theme.Theme;
 import com.atlauncher.injector.Injector;
 import com.atlauncher.injector.InjectorFactory;
-import com.atlauncher.managers.AccountManager;
 import com.atlauncher.managers.BenchmarkManager;
-import com.atlauncher.managers.InstanceManager;
-import com.atlauncher.managers.LanguageManager;
 import com.atlauncher.managers.LogManager;
-import com.atlauncher.managers.PackManager;
-import com.atlauncher.managers.SettingsManager;
-import com.atlauncher.utils.HTMLUtils;
-import com.atlauncher.utils.Utils;
 import io.github.asyncronous.toast.Toaster;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
-import javax.swing.InputMap;
-import javax.swing.JOptionPane;
-import javax.swing.KeyStroke;
-import javax.swing.SwingUtilities;
-import javax.swing.ToolTipManager;
-import javax.swing.UIManager;
-import javax.swing.text.DefaultEditorKit;
-import java.awt.Image;
-import java.awt.SystemTray;
-import java.awt.TrayIcon;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Method;
-import java.nio.file.Path;
-import java.util.Enumeration;
 import java.util.Locale;
-import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 /**
  * Main entry point for the application, Java runs the main method here when the application is launched.
@@ -93,14 +56,6 @@ public class App {
      * when there are possible issues in which the user may have to download the update manually.
      */
     public static boolean wasUpdated = false;
-
-    /**
-     * This controls if GZIP is used when downloading files through the launcher. It's used as a debugging tool and is
-     * enabled with the command line argument shown below.
-     * <p/>
-     * --usegzip=false
-     */
-    public static boolean useGzipForDownloads = true;
 
     /**
      * This allows skipping the system tray intergation so that the launcher doesn't even try to show the icon and menu
@@ -155,14 +110,18 @@ public class App {
      * This is the theme used by the launcher. By default it uses the default theme until the theme can be created and
      * loaded.
      * <p/>
-     * For more information on themeing, please see https://atl.pw/theme
+     * For more information on theming, please see https://atl.pw/theme
      */
     public static Theme THEME = Theme.DEFAULT_THEME;
 
     static {
-        /**
-         * Sets up where all uncaught exceptions go to.
-         */
+        // Set English as the default locale. CodeChickenLib(?) has some issues when not using this on some systems.
+        Locale.setDefault(Locale.ENGLISH);
+
+        // Prefer to use IPv4
+        System.setProperty("java.net.preferIPv4Stack", "true");
+
+        // Sets up where all uncaught exceptions go to.
         Thread.setDefaultUncaughtExceptionHandler(new ExceptionStrainer());
     }
 
@@ -173,16 +132,14 @@ public class App {
      */
     public static void main(String[] args) {
         BenchmarkManager.start();
-        // Set English as the default locale. CodeChickenLib(?) has some issues when not using this on some systems.
-        Locale.setDefault(Locale.ENGLISH);
 
-        // Prefer to use IPv4
-        System.setProperty("java.net.preferIPv4Stack", "true");
-
+        // Parse all the command line arguments
         parseCommandLineArguments(args);
 
+        // Start the loader
         Loader loader = new Loader();
 
+        // Check if the user is using the old OSX app
         loader.checkIfUsingOldOSXApp();
 
         // Load the theme and style everything.
@@ -191,8 +148,10 @@ public class App {
         // Load the console, making sure it's after the theme and L&F has been loaded otherwise bad results may occur.
         loader.loadConsole();
 
+        // Load the system tray if enabled
         loader.loadSystemTray();
 
+        // Log some basic information about the launcher and system to the console
         loader.logInformation();
 
         // Now for some Mac specific stuff, mainly just setting the name of the application and icon.
@@ -202,8 +161,10 @@ public class App {
         settings = new OldSettings();
         settings.loadEverything();
 
+        // Checks to see if the user has completed the first run dialog
         loader.checkIfSetupIsComplete();
 
+        // Checks if we're auto launching an instance or not
         loader.autoLaunchInstance();
 
         // See write launchers location to disk and check if the launch tool has been used and act upon it
@@ -212,7 +173,8 @@ public class App {
         // Finished loading, so remove splash screen and other work
         loader.finish();
 
-        App.frame = new LauncherFrame(); // Open the Launcher
+        // Open the launcher
+        App.frame = new LauncherFrame();
 
         BenchmarkManager.stop();
     }
@@ -223,7 +185,6 @@ public class App {
         parser.accepts("updated").withRequiredArg().ofType(Boolean.class);
         parser.accepts("debug").withRequiredArg().ofType(Boolean.class);
         parser.accepts("debug-level").withRequiredArg().ofType(Integer.class);
-        parser.accepts("use-gzip").withRequiredArg().ofType(Boolean.class);
         parser.accepts("skip-tray-integration").withRequiredArg().ofType(Boolean.class);
         parser.accepts("force-offline-mode").withRequiredArg().ofType(Boolean.class);
 
@@ -232,10 +193,9 @@ public class App {
         wasUpdated = options.has("updated") ? (Boolean) options.valueOf("updated") : false;
 
         if (options.has("debug")) {
-            LogManager.showDebug = (Boolean) options.valueOf("debug");
+            LogManager.showDebug = true;
             LogManager.debugLevel = 1;
-            LogManager.debug("Debug logging is enabled! Please note that this will remove any censoring of " + "user " +
-                    "data!");
+            LogManager.debug("Debug logging is enabled! Please note that this will remove any censoring of user data!");
         }
 
         if (options.has("debug-level")) {
@@ -243,19 +203,12 @@ public class App {
             LogManager.debug("Debug level has been set to " + options.valueOf("debug-level") + "!");
         }
 
-        useGzipForDownloads = options.has("use-gzip") ? (Boolean) options.valueOf("use-gzip") : true;
-        if (!useGzipForDownloads) {
-            LogManager.debug("GZip has been turned off for downloads! Don't ask for support with this " +
-                    "disabled!", true);
-        }
-
-        skipTrayIntegration = options.has("skip-tray-integration") ? (Boolean) options.valueOf
-                ("skip-tray-integration") : false;
+        skipTrayIntegration = options.has("skip-tray-integration");
         if (skipTrayIntegration) {
             LogManager.debug("Skipping tray integration!", true);
         }
 
-        forceOfflineMode = options.has("force-offline-mode") ? (Boolean) options.valueOf("force-offline-mode") : false;
+        forceOfflineMode = options.has("force-offline-mode");
         if (forceOfflineMode) {
             LogManager.debug("Forcing offline mode!", true);
         }
