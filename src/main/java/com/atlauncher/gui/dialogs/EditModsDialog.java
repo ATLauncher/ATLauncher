b2f788c6f862a737f12e0891317868359356a1cb
flaw600 <sikand@gmail.com>
2016-02-04 11:32:02 -0600

    Potential solution for #218 for the 3.3.0.0 branch

--- src/main/java/com/atlauncher/gui/dialogs/EditModsDialog.java
+++ src/main/java/com/atlauncher/gui/dialogs/EditModsDialog.java
@@ -131,11 +131,17 @@ public class EditModsDialog extends JDialog {
         addButton = new JButton(LanguageManager.localize("instance.addmod"));
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                FileChooserDialog fcd = new FileChooserDialog(LanguageManager.localize("instance.addmod"),
-                        LanguageManager.localize("common.mod"), LanguageManager.localize("common.add"), LanguageManager
-                        .localize("instance.typeofmod"), new String[]{"Mods Folder", "Inside Minecraft.jar",
-                        "CoreMods Mod", "Texture Pack", "Resource Pack", "Shader Pack"}, new String[]{"jar", "zip",
-                        "litemod"});
+                FileChooserDialog fcd = null;
+                if(Double.valueOf(instance.getMinecraftVersion().substring(0, 3)) < 1.6)
+                	fcd = new FileChooserDialog(LanguageManager.localize("instance.addmod"), LanguageManager
+                            .localize("common.mod"), LanguageManager.localize("common.add"), LanguageManager
+                            .localize("instance.typeofmod"), new String[]{"Mods Folder", "Inside Minecraft.jar",
+                            "CoreMods Mod", "Texture Pack", "Shader Pack"}, new String[]{"jar", "zip", "litemod"});
+                else
+                	new FileChooserDialog(LanguageManager.localize("instance.addmod"), LanguageManager
+                            .localize("common.mod"), LanguageManager.localize("common.add"), LanguageManager
+                            .localize("instance.typeofmod"), new String[]{"Mods Folder", "Inside Minecraft.jar",
+                            "Resource Pack", "Shader Pack"}, new String[]{"jar", "zip", "litemod"});
                 ArrayList<File> files = fcd.getChosenFiles();
                 if (files != null && files.size() >= 1) {
                     boolean reload = false;
