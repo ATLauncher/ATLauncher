2fe4273c1e234c45ee3709e08106dce54d7ea9e7
flaw600 <sikand@gmail.com>
2016-02-10 23:58:36 -0600

    Redoing of #218 per Ryan's request

--- src/main/java/com/atlauncher/gui/dialogs/EditModsDialog.java
+++ src/main/java/com/atlauncher/gui/dialogs/EditModsDialog.java
@@ -129,16 +129,24 @@ public class EditModsDialog extends JDialog {
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 FileChooserDialog fcd = null;
-                if(Double.valueOf(instance.getMinecraftVersion().substring(0, 3)) < 1.6)
+                boolean coreModsUsed = false;
+                try {
+                	instance.getCoreModsDirectory();
+                	coreModsUsed = true;
+                } catch (NullPointerException err) {
+                	coreModsUsed = false;
+                }
+                if(coreModsUsed) {
                 	fcd = new FileChooserDialog(Language.INSTANCE.localize("instance.addmod"), Language
                             .INSTANCE.localize("common.mod"), Language.INSTANCE.localize("common.add"), Language.INSTANCE
                             .localize("instance.typeofmod"), new String[]{"Mods Folder", "Inside Minecraft.jar",
                             "CoreMods Mod", "Texture Pack", "Shader Pack"}, new String[]{"jar", "zip", "litemod"});
-                else
+                } else {
                 	new FileChooserDialog(Language.INSTANCE.localize("instance.addmod"), Language
                             .INSTANCE.localize("common.mod"), Language.INSTANCE.localize("common.add"), Language.INSTANCE
                             .localize("instance.typeofmod"), new String[]{"Mods Folder", "Inside Minecraft.jar",
                             "Resource Pack", "Shader Pack"}, new String[]{"jar", "zip", "litemod"});
+                }
                 ArrayList<File> files = fcd.getChosenFiles();
                 if (files != null && files.size() >= 1) {
                     boolean reload = false;
