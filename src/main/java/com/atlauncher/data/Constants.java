718e36b0e61382df530131f8f3da8d1f9dc0556c
Ryan Dowling <RyanTheAllmighty@users.noreply.github.com>
2017-04-01 09:36:31 +1100

    Update Constants.java
    Update constants file

--- src/main/java/com/atlauncher/data/Constants.java
+++ src/main/java/com/atlauncher/data/Constants.java
@@ -25,14 +25,14 @@ public class Constants {
     public static final String PASTE_API_URL = "http://paste.atlauncher.com/api/create";
     public static final Server[] SERVERS = new Server[]{
             new Server("Auto", "download.nodecdn.net/containers/atl", true, false),
-            new Server("Backup Server", "anne.nodeservers.net:8080/containers/atl", false, false),
-            new Server("EU - Amsterdam 1", "bob.nodeservers.net/containers/atl", true, false),
-            new Server("EU - Amsterdam 2", "emma.nodeservers.net/containers/atl", true, false),
-            new Server("EU - Amsterdam 3", "lisa.nodeservers.net/containers/atl", true, false),
-            new Server("US East - Ashburn 1", "anne.nodeservers.net/containers/atl", true, false),
-            new Server("US East - Ashburn 2", "bruce.nodeservers.net/containers/atl", true, false),
-            new Server("US East - Ashburn 3", "dave.nodeservers.net/containers/atl", true, false),
-            new Server("US West - Phoenix 1", "adam.nodeservers.net/containers/atl", true, false),
+            new Server("Backup Server", "anne.nodecdn.net:8080/containers/atl", false, false),
+            new Server("EU - Amsterdam 1", "bob.nodecdn.net/containers/atl", true, false),
+            new Server("EU - Amsterdam 2", "emma.nodecdn.net/containers/atl", true, false),
+            new Server("EU - Amsterdam 3", "lisa.nodecdn.net/containers/atl", true, false),
+            new Server("US East - Ashburn 1", "anne.nodecdn.net/containers/atl", true, false),
+            new Server("US East - Ashburn 2", "bruce.nodecdn.net/containers/atl", true, false),
+            new Server("US East - Ashburn 3", "dave.nodecdn.net/containers/atl", true, false),
+            new Server("US West - Phoenix 1", "adam.nodecdn.net/containers/atl", true, false),
             new Server("Master Server (Testing Only)", "master.atlcdn.net", false, true)
     };
 }
