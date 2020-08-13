cccc6827cea173daf44880b414d7fdf1018e665d
Ryan Dowling <ryan@ryandowling.me>
2020-08-13 14:05:43 +1000

    fix: clean up getSystemInfo generateBinaries scripts

--- getSystemInfo/generateBinaries.bat
+++ getSystemInfo/generateBinaries.bat
@@ -1,6 +1,9 @@
-docker build -t atlauncher/getsysteminfo .
+del dist/*
+docker image rm atlauncher/getsysteminfo
+docker build --rm -t atlauncher/getsysteminfo .
 FOR /F "tokens=* USEBACKQ" %%F IN (`docker create -t atlauncher/getsysteminfo`) DO (
 SET IMAGE_ID=%%F
 )
 
 docker cp %IMAGE_ID%:/app/dist .
+docker rm %IMAGE_ID%
--- getSystemInfo/generateBinaries.sh
+++ getSystemInfo/generateBinaries.sh
@@ -1,4 +1,7 @@
-docker build -t atlauncher/getsysteminfo .
+del dist/*
+docker image rm atlauncher/getsysteminfo
+docker build --rm -t atlauncher/getsysteminfo .
 IMAGE_ID=`docker create -t atlauncher/getsysteminfo .`
 
 docker cp ${IMAGE_ID}:/app/dist .
+docker rm ${IMAGE_ID}
