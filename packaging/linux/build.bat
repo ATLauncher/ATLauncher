c697a8af360e9917936350b79f0de362e1cf83a7
Ryan Dowling <ryan@ryandowling.me>
2021-02-01 20:13:50 +1100

    fix: linux packaging build scripts

--- packaging/linux/build.bat
+++ packaging/linux/build.bat
@@ -1,6 +1,6 @@
 @REM @REM Deb packaging
 docker build -t atlauncher/atlauncher-packaging-linux-deb -f deb/Dockerfile .
-docker run --rm -i -v %cd%\out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../atlauncher atlauncher_1.0-1.deb
+docker run --rm -i -v %cd%\out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../atlauncher atlauncher-1.0-1.deb
 
 @REM RPM packaging
 docker build -t atlauncher/atlauncher-packaging-linux-rpm -f rpm/Dockerfile .
--- packaging/linux/build.sh
+++ packaging/linux/build.sh
@@ -1,6 +1,7 @@
-docker build -t atlauncher/debian .
-IMAGE_ID=`docker create -t atlauncher/debian .`
+# Deb packaging
+docker build -t atlauncher/atlauncher-packaging-linux-deb -f deb/Dockerfile .
+docker run --rm -i -v $PWD/out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../atlauncher atlauncher-1.0-1.deb
 
-mkdir out
-
-docker cp ${IMAGE_ID}:/work/atlauncher.deb ./out/atlauncher_1.0-1.deb
+# RPM packaging
+docker build -t atlauncher/atlauncher-packaging-linux-rpm -f rpm/Dockerfile .
+docker run --rm -i -v $PWD/out:/root/rpmbuild/RPMS/noarch -w /work atlauncher/atlauncher-packaging-linux-rpm rpmbuild -bb --build-in-place --target noarch atlauncher.spec
