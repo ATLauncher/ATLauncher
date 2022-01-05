7d4efd63e14dbe853b94ee4796a459914899844d
Ryan Dowling <ryan@ryandowling.me>
2022-01-05 16:42:12 +1100

    chore: hello new year

--- src/main/java/com/atlauncher/gui/tabs/Tab.java
+++ src/main/java/com/atlauncher/gui/tabs/Tab.java
@@ -1,6 +1,6 @@
 /*
  * ATLauncher - https://github.com/ATLauncher/ATLauncher
- * Copyright (C) 2013-2021 ATLauncher
+ * Copyright (C) 2013-2022 ATLauncher
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
