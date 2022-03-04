a0210f8d4442890343bffdad883fc6cecf7e0a2f
Ryan Dowling <ryan@ryandowling.me>
2022-03-05 01:01:20 +1100

    refactor: move migration message on launch earlier and add button to open move site

--- src/main/java/com/atlauncher/data/Instance.java
+++ src/main/java/com/atlauncher/data/Instance.java
@@ -757,6 +757,23 @@ public class Instance extends MinecraftVersion {
             }
         }
 
+        // Mojang accounts cannot login anymore, so unless playing offline, don't launch
+        if (account instanceof MojangAccount && !offline) {
+            LogManager.info("Account must be migrated to a Microsoft account before being able to play.");
+            int ret = DialogManager.okDialog().addOption(GetText.tr("Open Migration Site"), true)
+                    .setTitle(GetText.tr("Account Must Migrate"))
+                    .setContent(new HTMLBuilder().center().text(GetText.tr(
+                            "Mojang accounts no longer exist and must be migrated to a Microsoft account in order to continue playing.<br/><br/>Please visit https://minecraft.net/move and relogin to your account in the Accounts tab to continue to play."))
+                            .build())
+                    .setType(DialogManager.ERROR).show();
+
+            if (ret == 1) {
+                OS.openWebBrowser("https://minecraft.net/move");
+            }
+
+            return false;
+        }
+
         String playerName = account.minecraftUsername;
 
         if (offline) {
@@ -859,12 +876,6 @@ public class Instance extends MinecraftVersion {
                         session = new LoginResponse(mojangAccount.username);
                         session.setOffline();
                     } else {
-                        DialogManager.okDialog().setTitle(GetText.tr("Account Must Migrate"))
-                                .setContent(new HTMLBuilder().center().text(GetText.tr(
-                                        "Mojang accounts no longer exist and must be migrated to a Microsoft account in order to continue playing.<br/><br/>Please visit https://minecraft.net/move and relogin to your account in the Accounts tab to continue to play."))
-                                        .build())
-                                .setType(DialogManager.ERROR).show();
-
                         App.launcher.setMinecraftLaunched(false);
 
                         if (App.launcher.getParent() != null) {
