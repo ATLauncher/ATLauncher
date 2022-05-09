44df426472356015a553cc301e9a6e9645c5926a
Ryan Dowling <ryan@ryandowling.me>
2022-05-09 20:32:04 +1000

    fix: remove api key header from network debug logger

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,5 +8,6 @@ This changelog only contains the changes that are unreleased. For changes for in
 ### New Features
 
 ### Fixes
+- Remove api key header from network debug logger
 
 ### Misc
--- src/main/java/com/atlauncher/network/DebugLoggingInterceptor.java
+++ src/main/java/com/atlauncher/network/DebugLoggingInterceptor.java
@@ -36,6 +36,8 @@ public final class DebugLoggingInterceptor implements Interceptor {
         String debugLogMessage = request.toString();
         if (request.header("Authorization") != null) {
             debugLogMessage = debugLogMessage.replace(request.header("Authorization"), "REDACTED");
+        } else if (request.header("x-api-key") != null) {
+            debugLogMessage = debugLogMessage.replace(request.header("x-api-key"), "REDACTED");
         }
         LogManager.debug(debugLogMessage, 5);
 
