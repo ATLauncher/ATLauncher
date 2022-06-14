dcfde37026cf4cffeb22e7c79a8212925dc21c8d
Tazz <tasman.vose@gmail.com>
2022-06-14 14:18:41 -0600

    add Task & TaskEngine

---
+++ src/main/java/com/atlauncher/AppTaskEngine.java
@@ -0,0 +1,17 @@
+package com.atlauncher;
+
+import com.atlauncher.task.Task;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public final class AppTaskEngine{
+    static final ExecutorService pool = Executors.newWorkStealingPool();
+
+    private AppTaskEngine(){}
+
+    public static Future<?> submit(final Task task){
+        return pool.submit(task);
+    }
+}
---
+++ src/main/java/com/atlauncher/task/Task.java
@@ -0,0 +1,5 @@
+package com.atlauncher.task;
+
+public interface Task extends Runnable{
+
+}
