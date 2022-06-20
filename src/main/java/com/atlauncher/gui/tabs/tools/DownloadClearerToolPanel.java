4cbc62bbb00f98c797b1dfe57b859dadcd12f699
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2022-06-20 03:52:54 -0400

    [Architecture] ToolsTab (#612)
    * Move ToolsTab under tools/
    
    * Migrate DebugModePanel business code
    
    * Migrate DownloadClearerToolPanel business code
    
    * Migrate LogClearerToolPanel business code
    
    * Migrate NetworkCheckerToolPanel business code
    
    * Migrate RuntimeDownloaderToolPanel business code
    
    * Migrate SkinUpdaterToolPanel business code
    
    * Bind viewModel to panels in ToolsTab
    
    * misc: use function
    
    * Update license
    
    * Document and rename in IToolsViewModel
    
    * Move RuntimeDownloaderToolPanel button states to be handled by VM
    
    * Update CHANGELOG.md
    
    * fix: invert the inverted runtime downloader result check
    
    * misc: Move analytics calls to view model
    
    Co-authored-by: Ryan Dowling <ryan@ryandowling.me>

--- src/main/java/com/atlauncher/gui/tabs/tools/DownloadClearerToolPanel.java
+++ src/main/java/com/atlauncher/gui/tabs/tools/DownloadClearerToolPanel.java
@@ -17,55 +17,31 @@
  */
 package com.atlauncher.gui.tabs.tools;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-
 import javax.swing.JLabel;
 
 import org.mini2Dx.gettext.GetText;
 
-import com.atlauncher.FileSystem;
 import com.atlauncher.builders.HTMLBuilder;
 import com.atlauncher.managers.DialogManager;
-import com.atlauncher.network.Analytics;
-import com.atlauncher.utils.Utils;
 
 @SuppressWarnings("serial")
-public class DownloadClearerToolPanel extends AbstractToolPanel implements ActionListener {
+public class DownloadClearerToolPanel extends AbstractToolPanel {
 
-    public DownloadClearerToolPanel() {
+    public DownloadClearerToolPanel(IToolsViewModel viewModel) {
         super(GetText.tr("Download Clearer"));
 
         JLabel INFO_LABEL = new JLabel(new HTMLBuilder().center().split(70).text(GetText.tr(
                 "This tool clears out all the downloads done by the launcher. This will not affect any instances, but means new pack installs may take longer as it needs to redownload mods."))
-                .build());
+            .build());
         MIDDLE_PANEL.add(INFO_LABEL);
         BOTTOM_PANEL.add(LAUNCH_BUTTON);
-        LAUNCH_BUTTON.addActionListener(this);
-    }
+        LAUNCH_BUTTON.addActionListener(e -> {
+            if (e.getSource() == LAUNCH_BUTTON) {
+                viewModel.clearDownloads();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == LAUNCH_BUTTON) {
-            Analytics.sendEvent("DownloadClearer", "Run", "Tool");
-
-            for (File file : FileSystem.DOWNLOADS.toFile().listFiles()) {
-                if (!file.equals(FileSystem.TECHNIC_DOWNLOADS.toFile())) {
-                    Utils.delete(file);
-                }
-            }
-
-            for (File file : FileSystem.TECHNIC_DOWNLOADS.toFile().listFiles()) {
-                Utils.delete(file);
-            }
-
-            for (File file : FileSystem.FAILED_DOWNLOADS.toFile().listFiles()) {
-                Utils.delete(file);
-            }
-
-            DialogManager.okDialog().setType(DialogManager.INFO).setTitle(GetText.tr("Success"))
+                DialogManager.okDialog().setType(DialogManager.INFO).setTitle(GetText.tr("Success"))
                     .setContent(GetText.tr("Successfully cleared the downloads.")).show();
-        }
+            }
+        });
     }
 }
