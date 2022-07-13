39648d1469a28a62073dc0e92436dde388e1a10a
Ryan Dowling <ryan@ryandowling.me>
2022-07-13 21:09:37 +1000

    fix: compilation issue with Java 8

--- src/main/java/com/atlauncher/data/minecraft/loaders/forge/FMLLibrariesConstants.java
+++ src/main/java/com/atlauncher/data/minecraft/loaders/forge/FMLLibrariesConstants.java
@@ -17,23 +17,24 @@
  */
 package com.atlauncher.data.minecraft.loaders.forge;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FMLLibrariesConstants {
-    private final static List<FMLLibrary> libs13 = List.of(
+    private final static List<FMLLibrary> libs13 = Arrays.asList(
             new FMLLibrary("argo-2.25.jar", "bb672829fde76cb163004752b86b0484bd0a7f4b", 123642L),
             new FMLLibrary("guava-12.0.1.jar", "b8e78b9af7bf45900e14c6f958486b6ca682195f", 1795932L),
             new FMLLibrary("asm-all-4.0.jar", "98308890597acb64047f7e896638e0d98753ae82", 212767L));
 
-    private final static List<FMLLibrary> libs14 = List.of(
+    private final static List<FMLLibrary> libs14 = Arrays.asList(
             new FMLLibrary("argo-2.25.jar", "bb672829fde76cb163004752b86b0484bd0a7f4b", 123642L),
             new FMLLibrary("guava-12.0.1.jar", "b8e78b9af7bf45900e14c6f958486b6ca682195f", 1795932L),
             new FMLLibrary("asm-all-4.0.jar", "98308890597acb64047f7e896638e0d98753ae82", 212767L),
             new FMLLibrary("bcprov-jdk15on-147.jar", "b6f5d9926b0afbde9f4dbe3db88c5247be7794bb", 1997327L));
 
-    private final static List<FMLLibrary> libs15 = List.of(
+    private final static List<FMLLibrary> libs15 = Arrays.asList(
             new FMLLibrary("argo-small-3.2.jar", "58912ea2858d168c50781f956fa5b59f0f7c6b51", 91333L),
             new FMLLibrary("guava-14.0-rc3.jar", "931ae21fa8014c3ce686aaa621eae565fefb1a6a", 2189140L),
             new FMLLibrary("asm-all-4.1.jar", "054986e962b88d8660ae4566475658469595ef58", 214592L),
@@ -41,7 +42,7 @@ public class FMLLibrariesConstants {
             new FMLLibrary("deobfuscation_data_1.5.zip", "5f7c142d53776f16304c0bbe10542014abad6af8", 200547L),
             new FMLLibrary("scala-library.jar", "458d046151ad179c85429ed7420ffb1eaf6ddf85", 7114640L));
 
-    private final static List<FMLLibrary> libs151 = List.of(
+    private final static List<FMLLibrary> libs151 = Arrays.asList(
             new FMLLibrary("argo-small-3.2.jar", "58912ea2858d168c50781f956fa5b59f0f7c6b51", 91333L),
             new FMLLibrary("guava-14.0-rc3.jar", "931ae21fa8014c3ce686aaa621eae565fefb1a6a", 2189140L),
             new FMLLibrary("asm-all-4.1.jar", "054986e962b88d8660ae4566475658469595ef58", 214592L),
@@ -49,7 +50,7 @@ public class FMLLibrariesConstants {
             new FMLLibrary("deobfuscation_data_1.5.1.zip", "22e221a0d89516c1f721d6cab056a7e37471d0a6", 200886L),
             new FMLLibrary("scala-library.jar", "458d046151ad179c85429ed7420ffb1eaf6ddf85", 7114640L));
 
-    private final static List<FMLLibrary> libs152 = List.of(
+    private final static List<FMLLibrary> libs152 = Arrays.asList(
             new FMLLibrary("argo-small-3.2.jar", "58912ea2858d168c50781f956fa5b59f0f7c6b51", 91333L),
             new FMLLibrary("guava-14.0-rc3.jar", "931ae21fa8014c3ce686aaa621eae565fefb1a6a", 2189140L),
             new FMLLibrary("asm-all-4.1.jar", "054986e962b88d8660ae4566475658469595ef58", 214592L),
