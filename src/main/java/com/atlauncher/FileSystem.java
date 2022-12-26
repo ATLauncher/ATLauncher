5f4df2a2b7682ebf462effdd020fb84d797d7cac
doomsdayrs <doomsdayrs@gmail.com>
2022-12-25 23:07:08 -0500

    Fix isPortableCheck

--- src/main/java/com/atlauncher/FileSystem.java
+++ src/main/java/com/atlauncher/FileSystem.java
@@ -432,6 +432,7 @@ public final class FileSystem {
     public static boolean isPortablePopulated() {
         if (!useXdg()) {
             return Files.exists(FileSystem.BASE_DIR)
+                && Files.notExists(FileSystem.CONFIGS)
                 && FileSystem.BASE_DIR.toFile().listFiles().length > 2;
         }
         return false;
