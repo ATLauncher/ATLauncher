38eac19f709ec016b2deecf77c01cb5b4592f815
doomsdayrs <doomsdayrs@gmail.com>
2023-02-14 11:50:31 -0500

    Convert main build.gradle to kts
    I dropped jitpack from plugins as upstream gettext
     has all patches in. Also, because ben-manes versions does too.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,21 @@
+import org.gradle.api.internal.FactoryNamedDomainObjectContainer
+import org.jetbrains.kotlin.de.undercouch.gradle.tasks.download.Download
+import org.mini2Dx.gettext.plugin.GetTextSource
 import java.text.SimpleDateFormat
+import java.util.*
 
 buildscript {
     repositories {
         mavenCentral()
-        maven {
-            url "https://jitpack.io"
-            content {
-                includeGroup "com.github.RyanTheAllmighty.gettext"
-            }
-        }
     }
     dependencies {
-        classpath "com.github.RyanTheAllmighty.gettext:gettext-gradle-plugin:aab5c30bf8"
+        classpath("org.mini2Dx:gettext-gradle-plugin:1.11.0")
     }
 }
 
 plugins {
-    id("java")
-    id("application")
+    java
+    application
 
     id("org.cadixdev.licenser") version "0.6.1"
     id("com.adarshr.test-logger") version "3.2.0"
@@ -25,30 +23,28 @@ plugins {
     id("edu.sc.seis.launch4j") version "2.5.3"
     id("de.undercouch.download") version "5.1.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.github.ben-manes.versions") version "0.45.0"
     id("org.jetbrains.kotlin.jvm") version "1.8.0"
     id("com.apollographql.apollo") version "2.5.14"
 }
 
-apply(plugin: "org.mini2Dx.gettext")
+apply(plugin = "org.mini2Dx.gettext")
 
-sourceCompatibility = "1.8"
-targetCompatibility = "1.8"
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 group = "com.atlauncher"
-version = rootProject.file("src/main/resources/version").text.trim().replace(".Beta", "")
+version = rootProject.file("src/main/resources/version").readText().trim().replace(".Beta", "")
 
 repositories {
     mavenCentral()
     gradlePluginPortal()
-    maven {
-        url "https://libraries.minecraft.net"
-    }
-    maven {
-        url "https://jitpack.io"
+    maven("https://libraries.minecraft.net")
+    maven("https://jitpack.io") {
         content {
-            includeGroup "com.github.RyanTheAllmighty.gettext"
-            includeGroup "com.github.Vatuu"
+            includeGroup("com.github.Vatuu")
         }
     }
 }
@@ -68,7 +64,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp-tls:4.9.3")
     implementation("net.mikehardy:google-analytics-java:2.0.11")
     implementation("io.sentry:sentry:6.1.4")
-    implementation("com.github.RyanTheAllmighty.gettext:gettext-lib:88ae68d897")
+    implementation("org.mini2Dx:gettext-lib:1.11.0")
     implementation("org.apache.logging.log4j:log4j-api:2.17.2")
     implementation("org.apache.logging.log4j:log4j-core:2.17.2")
     implementation("com.sangupta:murmur:1.0.0")
@@ -101,50 +97,53 @@ dependencies {
 }
 
 application {
-    mainClass = "com.atlauncher.App"
-    applicationDefaultJvmArgs = [
+    mainClass.set("com.atlauncher.App")
+    applicationDefaultJvmArgs = listOf(
         "-Djna.nosys=true",
         "-Djava.net.preferIPv4Stack=true",
         "-Dawt.useSystemAAFontSettings=on",
         "-Dswing.aatext=true"
-    ]
+    )
 }
 
-tasks.named("test") {
+tasks.test {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 
     testlogger {
-        theme "mocha"
+        setTheme("mocha")
     }
 }
 
-jar {
+tasks.jar {
     manifest {
         attributes(
-            "SplashScreen-Image": "/assets/image/splash-screen.png",
-            "Implementation-Title": project.name,
-            "Implementation-Version": archiveVersion,
-            "Implementation-Vender": "ATLauncher",
-            "Main-Class": "com.atlauncher.App",
-            "Multi-Release": "true"
+            "SplashScreen-Image" to "/assets/image/splash-screen.png",
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to archiveVersion,
+            "Implementation-Vender" to "ATLauncher",
+            "Main-Class" to "com.atlauncher.App",
+            "Multi-Release" to "true"
         )
     }
 }
 
 apollo {
-    customTypeMapping = [
-        "ID"      : "java.lang.String",
-        "DateTime": "java.util.Date"
-    ]
-    packageName = "com.atlauncher.graphql"
+    customTypeMapping.set(
+        mapOf(
+            "ID" to "java.lang.String",
+            "DateTime" to "java.util.Date"
+        )
+    )
+    @Suppress("OPT_IN_USAGE") // It's experimental, and that is fine
+    packageName.set("com.atlauncher.graphql")
 }
 
-gettext {
-    translations {
+extensions.configure<FactoryNamedDomainObjectContainer<GetTextSource>>("gettext") {
+    create("translations") {
         srcDir = "src"
         include = "main/java/com/atlauncher/**/*.java"
-        excludes = [
+        excludes = arrayOf(
             "main/java/com/atlauncher/adapter/**/*.java",
             "main/java/com/atlauncher/annot/**/*.java",
             "main/java/com/atlauncher/collection/**/*.java",
@@ -153,14 +152,14 @@ gettext {
             "main/java/com/atlauncher/interfaces/**/*.java",
             "main/java/com/atlauncher/listener/**/*.java",
             "main/java/com/atlauncher/utils/**/*.java"
-        ]
+        )
         commentFormat = " #. "
         outputFilename = "translations.pot"
     }
 }
 
 license {
-    header = project.file("LICENSEHEADER")
+    header(project.file("LICENSEHEADER"))
     include("**/*.java")
     exclude("io/github/**/*.java")
     exclude("net/minecraft/**/*.java")
@@ -168,12 +167,13 @@ license {
     exclude("com/atlauncher/gui/layouts/WrapLayout.java")
     newLine.set(false)
     properties {
-        year = currentYear()
+        set("year", currentYear())
     }
 }
 
-shadowJar {
-    classifier = null
+
+tasks.shadowJar {
+    archiveClassifier.set(null as String?) // type problem shenanigans
     minimize {
         exclude(dependency("org.apache.logging.log4j:.*:.*"))
         exclude(dependency("com.formdev:.*:.*"))
@@ -195,7 +195,7 @@ shadowJar {
     exclude("jni/i386-SunOS/")
     exclude("jni/Darwin/")
 
-    classifier = ""
+    archiveClassifier.set("")
 }
 
 macAppBundle {
@@ -205,65 +205,70 @@ macAppBundle {
     runtimeConfigurationName = "shadow"
     jarTask = "shadowJar"
     icon = "src/main/resources/assets/image/icon.icns"
-    javaProperties.put("user.dir", "\$APP_ROOT/Contents/Java")
-    javaProperties.put("apple.laf.useScreenMenuBar", "true")
-    javaExtras.put("-Djna.nosys", "true")
-    javaExtras.put("-Djava.net.preferIPv4Stack", "true")
-    javaExtras.put("-Dawt.useSystemAAFontSettings", "on")
-    javaExtras.put("-Dswing.aatext", "true")
-    bundleExtras.put("JVMVersion", project.targetCompatibility.toString() + "+")
+    javaProperties["user.dir"] = "\$APP_ROOT/Contents/Java"
+    javaProperties["apple.laf.useScreenMenuBar"] = "true"
+    javaExtras["-Djna.nosys"] = "true"
+    javaExtras["-Djava.net.preferIPv4Stack"] = "true"
+    javaExtras["-Dawt.useSystemAAFontSettings"] = "on"
+    javaExtras["-Dswing.aatext"] = "true"
+    bundleExtras["JVMVersion"] = project.java.targetCompatibility.toString() + "+"
 }
 
-copyToResourcesJava {
+tasks.copyToResourcesJava {
     rename("ATLauncher-${project.version}.jar", "ATLauncher.jar")
 }
 
-def currentYear() {
-    def df = new SimpleDateFormat("yyyy")
-    df.setTimeZone(TimeZone.getTimeZone("UTC"))
-    return df.format(new Date())
+fun currentYear(): String {
+    val df = SimpleDateFormat("yyyy")
+    df.timeZone = TimeZone.getTimeZone("UTC")
+    return df.format(Date())
 }
 
 launch4j {
     outfile = "ATLauncher-${project.version}.exe"
-    jreMinVersion = "${project.targetCompatibility.toString()}"
+    jreMinVersion = project.java.targetCompatibility.toString()
     mainClassName = "com.atlauncher.App"
     icon = "${projectDir}/src/main/resources/assets/image/icon.ico"
     version = "${project.version}"
     textVersion = "${project.version}"
     copyright = "2013-${currentYear()} ${project.name}"
-    companyName = "${project.name}"
+    companyName = project.name
     bundledJrePath = "jre/;%JAVA_HOME%;%PATH%"
-    jvmOptions = [
+    jvmOptions = setOf(
         "-Djna.nosys=true",
         "-Djava.net.preferIPv4Stack=true",
         "-Dawt.useSystemAAFontSettings=on",
         "-Dswing.aatext=true"
-    ]
+    )
+}
+
+fun getCopyPath(newSuffix: String, newLibs: String): File {
+    val archivePath = project.tasks.jar.get().archiveFile.get().asFile.path
+    return file(archivePath.replace(".jar", newSuffix).replace("libs", newLibs))
 }
 
 artifacts {
-    archives(shadowJar)
-    archives(file(project.tasks.jar.getArchivePath().getPath().replace(".jar", ".exe").replace("libs", "launch4j")))
-    archives(file(project.tasks.jar.getArchivePath().getPath().replace(".jar", ".zip").replace("libs", "distributions")))
+    archives(tasks.shadowJar)
+    archives(getCopyPath(".exe", "launch4j"))
+    archives(getCopyPath(".zip", "distributions"))
 }
 
-test {
-    if (JavaVersion.current().isJava9Compatible()) {
+tasks.test {
+    if (JavaVersion.current().isJava9Compatible) {
         jvmArgs("--add-opens=java.base/sun.security.x509=ALL-UNNAMED")
     }
 }
 
-tasks.register("copyArtifacts", Copy) {
-    dependsOn(build)
-    from(shadowJar)
-    from(file(project.tasks.jar.getArchivePath().getPath().replace(".jar", ".exe").replace("libs", "launch4j")))
-    from(file(project.tasks.jar.getArchivePath().getPath().replace(".jar", ".zip").replace("libs", "distributions")))
+tasks.register<Copy>("copyArtifacts") {
+    dependsOn(tasks.build)
+    from(tasks.shadowJar)
+    from(getCopyPath(".exe", "launch4j"))
+    from(getCopyPath(".zip", "distributions"))
     into("${projectDir}/dist")
 }
 
-tasks.register("downloadNewerUniversalJavaApplicationStub", Download) {
-    description "Downloads newer universalJavaApplicationStub"
+tasks.register<Download>("downloadNewerUniversalJavaApplicationStub") {
+    description = "Downloads newer universalJavaApplicationStub"
     src("https://raw.githubusercontent.com/tofi86/universalJavaApplicationStub/2dbbf92b35e61194266c985c8bc6b411053a1b4a/src/universalJavaApplicationStub")
     dest(file("$buildDir/macApp/${project.name}.app/Contents/MacOS/universalJavaApplicationStub"))
     overwrite(true)
@@ -273,43 +278,69 @@ tasks.register("createTestLauncherDir") {
     project.file("testLauncher/dev").mkdirs()
 }
 
-tasks.register("createMacApp", Zip) {
-    dependsOn(createApp, shadowJar)
+tasks.register<Zip>("createMacApp") {
+    dependsOn(tasks.named("createApp"), tasks.shadowJar)
     from("$buildDir/macApp") {
         include("${project.name}.app/**")
         exclude("${project.name}.app/Contents/MacOS")
     }
     from("$buildDir/macApp") {
         include("${project.name}.app/Contents/MacOS/**")
-        fileMode(0777)
+        fileMode = 0x777
     }
-    archiveName = "${project.name}-${project.version}.zip"
+    archiveFileName.set("${project.name}-${project.version}.zip")
 }
 
-copyArtifacts.finalizedBy {
-    println("ATLauncher has been built. Distribution files are located in the dist directory.")
+tasks.create("copyArtifactsFinal") {
+    doFirst {
+        println("ATLauncher has been built. Distribution files are located in the dist directory.")
+    }
 }
 
-clean.doFirst {
-    delete("${projectDir}/dist")
+tasks.named("copyArtifacts") {
+    finalizedBy("copyArtifactsFinal")
+}
+
+tasks.clean {
+    doFirst {
+        delete("${projectDir}/dist")
+    }
 }
 
 project.afterEvaluate {
     tasks.check {
-        dependsOn -= tasks.find {
-            it.name.equals("checkLicenses")
-        }
+        /*
+        Trick gradle by not "removing" a task
+         (which throws an unsupported error),
+         Instead filter current tasks and then set the new list.
+        */
+        setDependsOn(
+            dependsOn.filterNot {
+                (it as? Task)?.name == "checkLicenses"
+            }
+        )
     }
 }
 
-def shouldIgnoreUpdate = { String version -> return ["ALPHA", "BETA", "RC", "-M"].any { it -> version.toUpperCase().contains(it) } }
-tasks.named("dependencyUpdates").configure {
+fun getShouldIgnoreUpdate(version: String): Boolean {
+    return arrayOf("ALPHA", "BETA", "RC", "-M").any { version.toUpperCase() == (it) }
+}
+
+tasks.dependencyUpdates {
     rejectVersionIf {
-        shouldIgnoreUpdate(it.candidate.version)
+        getShouldIgnoreUpdate(candidate.version)
     }
 }
 
-build.finalizedBy(copyArtifacts)
-shadowJar.dependsOn(jar)
-build.dependsOn(createExe, createMacApp)
-createApp.finalizedBy(downloadNewerUniversalJavaApplicationStub)
+tasks {
+    build {
+        finalizedBy("copyArtifacts")
+        dependsOn(createExe, project.tasks.named("createMacApp"))
+    }
+    shadowJar {
+        dependsOn(project.tasks.jar)
+    }
+    createApp {
+        finalizedBy("downloadNewerUniversalJavaApplicationStub")
+    }
+}
