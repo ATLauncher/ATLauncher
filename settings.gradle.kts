e5a25d41c41670963a4c406175d4689129e9cb5c
doomsdayrs <doomsdayrs@gmail.com>
2023-02-14 10:10:29 -0500

    Convert settings.gradle to kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,9 @@
  * Detailed information about configuring a multi-project build in Gradle can be found
  * in the user manual at https://docs.gradle.org/7.1.1/userguide/multi_project_builds.html
  */
-rootProject.name = name
+// TODO simplify this
+val props = java.util.Properties()
+props.load(File("gradle.properties").reader())
+rootProject.name = props["name"] as String
 include("app")
 include("legacy-launch")
