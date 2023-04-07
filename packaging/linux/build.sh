22dc0937227bc57ad4190ed6d8f771a6b177385c
Ryan Dowling <ryan@ryandowling.me>
2023-04-07 21:44:52 +1000

    chore: update package scripts for new deb/rpm

--- packaging/linux/build.sh
+++ packaging/linux/build.sh
@@ -1,6 +1,6 @@
 # Deb packaging
 docker build -t atlauncher/atlauncher-packaging-linux-deb -f deb/Dockerfile .
-docker run --rm -i -v $PWD/out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../atlauncher atlauncher-1.0-1.deb
+docker run --rm -i -v $PWD/out:/work/out -w /work/out atlauncher/atlauncher-packaging-linux-deb dpkg-deb --build ../atlauncher atlauncher-1.4-1.deb
 
 # RPM packaging
 docker build -t atlauncher/atlauncher-packaging-linux-rpm -f rpm/Dockerfile .
