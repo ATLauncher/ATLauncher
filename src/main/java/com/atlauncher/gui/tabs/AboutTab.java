0f1e51d2b095204eecd3926d10c0cabd20c1699b
doomsdayrs <doomsdayrs@gmail.com>
2023-04-16 13:24:35 -0400

    Remove new line from license in AboutTab

--- src/main/java/com/atlauncher/gui/tabs/AboutTab.java
+++ src/main/java/com/atlauncher/gui/tabs/AboutTab.java
@@ -236,17 +236,17 @@ public class AboutTab extends JPanel implements Tab, RelocalizationListener {
                     license.setText("ATLauncher - https://github.com/ATLauncher/ATLauncher\n" +
                         "Copyright (C) 2013-2023 ATLauncher\n" +
                         "\n" +
-                        "This program is free software: you can redistribute it and/or modify\n" +
-                        "it under the terms of the GNU General Public License as published by\n" +
-                        "the Free Software Foundation, either version 3 of the License, or\n" +
+                        "This program is free software: you can redistribute it and/or modify " +
+                        "it under the terms of the GNU General Public License as published by " +
+                        "the Free Software Foundation, either version 3 of the License, or " +
                         "(at your option) any later version.\n" +
                         "\n" +
-                        "This program is distributed in the hope that it will be useful,\n" +
-                        "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
-                        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n" +
+                        "This program is distributed in the hope that it will be useful, " +
+                        "but WITHOUT ANY WARRANTY; without even the implied warranty of " +
+                        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the " +
                         "GNU General Public License for more details.\n" +
                         "\n" +
-                        "You should have received a copy of the GNU General Public License\n" +
+                        "You should have received a copy of the GNU General Public License " +
                         "along with this program. If not, see <http://www.gnu.org/licenses/>.\n");
                     JScrollPane scrollPane = new JScrollPane(license);
                     licensePanel.add(scrollPane);
