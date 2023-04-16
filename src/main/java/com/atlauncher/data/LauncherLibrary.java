92cb621e5e3c9091b994171933af20a0e4bd23d6
doomsdayrs <doomsdayrs@gmail.com>
2023-04-16 13:08:40 -0400

    Add Nonnull annotations to LauncherLibrary

--- src/main/java/com/atlauncher/data/LauncherLibrary.java
+++ src/main/java/com/atlauncher/data/LauncherLibrary.java
@@ -2,6 +2,10 @@ package com.atlauncher.data;
 
 import java.net.URI;
 
+import javax.annotation.Nonnull;
+
+import org.jetbrains.annotations.NotNull;
+
 /**
  * 09 / 04 / 2023
  * <p>
@@ -11,18 +15,20 @@ public class LauncherLibrary {
     /**
      * Name of the library
      */
+    @Nonnull
     public final String name;
 
     /**
      * Link to the library
      */
+    @Nonnull
     public final URI link;
 
     /**
      * @param name of the library
      * @param link to the library
      */
-    public LauncherLibrary(String name, URI link) {
+    public LauncherLibrary(@NotNull String name, @NotNull URI link) {
         this.name = name;
         this.link = link;
     }
