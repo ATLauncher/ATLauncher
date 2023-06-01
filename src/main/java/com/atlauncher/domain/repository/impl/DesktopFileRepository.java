1a11d916030bf27356e3cde16b0e7ec807c253d1
doomsdayrs <doomsdayrs@gmail.com>
2023-05-31 20:00:12 -0400

    Establish Source of Truth for repositories

---
+++ src/main/java/com/atlauncher/domain/repository/impl/DesktopFileRepository.java
@@ -0,0 +1,24 @@
+package com.atlauncher.domain.repository.impl;
+
+import com.atlauncher.data.Pack;
+import com.atlauncher.domain.repository.base.IDesktopFileRepository;
+
+/**
+ * 31 / 05 / 2023
+ */
+public class DesktopFileRepository extends IDesktopFileRepository {
+    @Override
+    public boolean addDesktopFile(Pack pack) {
+        return false;
+    }
+
+    @Override
+    public boolean removeDesktopFile(Pack pack) {
+        return false;
+    }
+
+    @Override
+    public boolean hasDesktopFile(Pack pack) {
+        return false;
+    }
+}
