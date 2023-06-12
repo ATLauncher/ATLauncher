69f7ff59234aa7f97f09983ddc2279960d80f60f
doomsdayrs <doomsdayrs@gmail.com>
2023-06-11 20:16:35 -0400

    Add clearXDG script for development clearing of xdg dirs

---
+++ clearXDG.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Created by @doomsdayrs
+# Clear out xdg directories
+
+echo "rm ~/.config/ATLauncher/"
+rm -Ir ~/.config/ATLauncher/
+
+echo "rm ~/.local/share/ATLauncher/"
+rm -Ir ~/.local/share/ATLauncher/
+
+echo "rm ~/.cache/ATLauncher/"
+rm -Ir ~/.cache/ATLauncher/
