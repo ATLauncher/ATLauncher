5c2ff2b29c9b48e9ad20d86d20da5d1bd9a5e93f
doomsdayrs <doomsdayrs@gmail.com>
2024-05-22 22:35:00 -0400

    Remove `userdata.json` conversion code.
    It relied on MojangAccount, todaloo!

--- src/main/java/com/atlauncher/managers/AccountManager.java
+++ src/main/java/com/atlauncher/managers/AccountManager.java
@@ -17,12 +17,10 @@
  */
 package com.atlauncher.managers;
 
-import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
@@ -42,12 +40,9 @@ import com.atlauncher.App;
 import com.atlauncher.FileSystem;
 import com.atlauncher.Gsons;
 import com.atlauncher.data.AbstractAccount;
-import com.atlauncher.data.Account;
 import com.atlauncher.data.MicrosoftAccount;
-import com.atlauncher.data.MojangAccount;
 import com.atlauncher.network.Analytics;
 import com.atlauncher.network.analytics.AnalyticsEvent;
-import com.atlauncher.utils.Utils;
 import com.google.gson.JsonIOException;
 import com.google.gson.reflect.TypeToken;
 
@@ -93,11 +88,6 @@ public class AccountManager {
         PerformanceManager.start();
         LogManager.debug("Loading accounts");
 
-        if (Files.exists(FileSystem.USER_DATA)) {
-            LogManager.info("Converting old account format to new format.");
-            convertAccounts();
-        }
-
         ArrayList<AbstractAccount> newAccounts = new ArrayList<>();
 
         if (Files.exists(FileSystem.ACCOUNTS)) {
@@ -129,52 +119,6 @@ public class AccountManager {
 
         LogManager.debug("Finished loading accounts");
         PerformanceManager.end();
-    }
-
-    /**
-     * Converts the saved Accounts from old format to new one
-     */
-    private static void convertAccounts() {
-        FileInputStream in = null;
-        ObjectInputStream objIn = null;
-        List<AbstractAccount> convertedAccounts = new LinkedList<>();
-        try {
-            in = new FileInputStream(FileSystem.USER_DATA.toFile());
-            objIn = new ObjectInputStream(in);
-            Object obj;
-            while ((obj = objIn.readObject()) != null) {
-                Account account = (Account) obj;
-
-                convertedAccounts.add(new MojangAccount(account.username, account.password, account.minecraftUsername,
-                        account.uuid, account.remember, account.clientToken, account.store));
-            }
-        } catch (EOFException e) {
-            // Don't log this, it always happens when it gets to the end of the file
-        } catch (IOException | ClassNotFoundException e) {
-            LogManager.logStackTrace("Exception while trying to convert accounts from file.", e);
-        } finally {
-            try {
-                if (objIn != null) {
-                    objIn.close();
-                }
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException e) {
-                LogManager.logStackTrace(
-                        "Exception while trying to close FileInputStream/ObjectInputStream when reading in " + ""
-                                + "accounts.",
-                        e);
-            }
-        }
-
-        saveAccounts(convertedAccounts);
-
-        try {
-            Files.delete(FileSystem.USER_DATA);
-        } catch (IOException e) {
-            LogManager.logStackTrace("Exception trying to remove old userdata file after conversion.", e);
-        }
     }
 
     public static void saveAccounts() {
