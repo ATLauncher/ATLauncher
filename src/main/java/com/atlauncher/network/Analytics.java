f4549d8a579b45bfe6e235bd992bf80f6f256abf
clocks <doomsdayrs@gmail.com>
2024-06-11 20:04:59 -0400

    Use putAll

--- src/main/java/com/atlauncher/network/Analytics.java
+++ src/main/java/com/atlauncher/network/Analytics.java
@@ -117,9 +117,7 @@ public final class Analytics {
     private static Map<String, Object> getEventProps(AnalyticsEvent event) {
         Map<String, Object> props = new HashMap<>();
 
-        for (Entry<String, Object> payloadSet : event.payload.entrySet()) {
-            props.put(payloadSet.getKey(), payloadSet.getValue());
-        }
+        props.putAll(event.payload);
 
         props.put("session_id", sessionId);
         props.put("distinct_id", App.settings.analyticsClientId);
