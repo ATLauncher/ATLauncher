48d8980a56e446e49c5da053431982d2c1772d38
clocks <doomsdayrs@gmail.com>
2024-08-04 12:25:30 -0400

    Add icon to WorkerInfoPanel

--- src/main/java/com/atlauncher/gui/components/WorkerInfoPanel.java
+++ src/main/java/com/atlauncher/gui/components/WorkerInfoPanel.java
@@ -3,13 +3,14 @@ package com.atlauncher.gui.components;
 import java.awt.BorderLayout;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
-import javax.swing.JTextField;
 
 import org.mini2Dx.gettext.GetText;
 
 import com.atlauncher.data.WorkerInfo;
+import com.atlauncher.utils.Utils;
 
 /**
  * @since 2024 / 06 / 10
@@ -18,7 +19,7 @@ public class WorkerInfoPanel extends JPanel {
 
     WorkerInfoPanel(WorkerInfo workerInfo, Runnable onStop) {
         setLayout(new BorderLayout());
-        add(new JTextField(workerInfo.name), BorderLayout.PAGE_START);
+        add(new JLabel(workerInfo.name, Utils.getIconImage(workerInfo.icon), JLabel.LEFT), BorderLayout.PAGE_START);
         add(new JProgressBar(), BorderLayout.CENTER);
 
         JButton stopButton = new JButton(GetText.tr("Stop"));
