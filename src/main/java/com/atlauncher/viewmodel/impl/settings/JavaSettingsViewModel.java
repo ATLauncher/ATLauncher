a79a3e9b64488b8b00de177128b4f08339cf361e
Sem Koolen <sem.koolen@gmail.com>
2025-01-26 13:25:50 +0100

    Refactored default settings to viewmodel.

--- src/main/java/com/atlauncher/viewmodel/impl/settings/JavaSettingsViewModel.java
+++ src/main/java/com/atlauncher/viewmodel/impl/settings/JavaSettingsViewModel.java
@@ -69,7 +69,8 @@ public class JavaSettingsViewModel implements SettingsListener {
             _useJavaProvidedByMinecraft = BehaviorSubject.create(),
             _disableLegacyLaunching = BehaviorSubject.create(),
             _useSystemGlfw = BehaviorSubject.create(),
-            _useSystemOpenAl = BehaviorSubject.create();
+            _useSystemOpenAl = BehaviorSubject.create(),
+            _useDedicatedGpu = BehaviorSubject.create();
 
     private final BehaviorSubject<CheckState> javaPathCheckState = BehaviorSubject.create(),
             javaParamCheckState = BehaviorSubject.create();
@@ -180,6 +181,10 @@ public class JavaSettingsViewModel implements SettingsListener {
         _disableLegacyLaunching.onNext(App.settings.disableLegacyLaunching);
         _useSystemGlfw.onNext(App.settings.useSystemGlfw);
         _useSystemOpenAl.onNext(App.settings.useSystemOpenAl);
+
+        if (OS.isLinux()) {
+            _useDedicatedGpu.onNext(App.settings.useDedicatedGpu);
+        }
     }
 
     /**
@@ -459,6 +464,15 @@ public class JavaSettingsViewModel implements SettingsListener {
 
     public void setSystemOpenAL(Boolean b) {
         App.settings.useSystemOpenAl = b;
+        SettingsManager.post();
+    }
+
+    public Observable<Boolean> getDedicatedGpu() {
+        return _useDedicatedGpu.observeOn(SwingSchedulers.edt());
+    }
+
+    public void setDedicatedGpu(Boolean b) {
+        App.settings.useDedicatedGpu = b;
         SettingsManager.post();
     }
 
